b619d32fbd2a137553fd50403eb85f0bf9022ad7
karol tatiana santa suarez <karoltatiana6780@gmail.com>
2026-04-08 13:19:27 -0500

    cambios al menu y al diseño

--- js/codigoRepetido/menu.js
+++ js/codigoRepetido/menu.js
@@ -6,15 +6,41 @@ class header extends HTMLElement{
     connectedCallback(){
         this.innerHTML =`
                 
-                
-        <header class="header">
-          <h1>nombre de la empresa o proyecto</h1>
-          <ul>
-            <li><a href="index.html">Inicio</a></li>
-            <li><a href="inicioDeSesion.html">Inicio de sesión</a></li>
-            <li><a href="registro.html">Registro de ejemplos</a></li>
-          </ul>
-        </header>
+
+        <!-- boostrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
+
+
+
+          <header>       
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">MJBE Bikes</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarScroll">
+                  <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="inicioDeSesion.html">Inicio de sesión</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="registro.html">Registro</a>
+                    </li>
+                  </ul>
+                  <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Búsqueda" aria-label="Search"/>
+                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                  </form>
+                </div>
+              </div>
+            </nav>
+          </header>
 
         `;
     }
